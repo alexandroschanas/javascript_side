6581eb8d38886e68ad0485b39dbf20a0f0846421
Your Name <you@example.com>
2025-01-23 17:52:05 +0200

    Changes to js file

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 let playButton = document.querySelector('.player__button toggle');
 let videoElement = document.querySelector('.player__video');
+let isPlaying = false;
 
 playButton.addEventListener('click',function(){
-
+    if(!isPlaying){
+        videoElement.play();
+    }else{
+        videoElement.pause();
+    }
 })
