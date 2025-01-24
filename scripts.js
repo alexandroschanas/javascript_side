de0c93d8235b127594f1e572d6e1db5b286e9217
Your Name <you@example.com>
2025-01-24 16:30:19 +0200

    Setting up the speed control element

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let playButton = document.querySelector('.player__button.toggle');
 let videoElement = document.querySelector('.player__video');
 let volumeControl = document.querySelector("input[name='volume']")
+let speedControl = document.querySelector("input[name='playbackRate']")
 let isPlaying = true;
 
 playButton.addEventListener('click',function(){
@@ -16,4 +17,9 @@ playButton.addEventListener('click',function(){
 volumeControl.addEventListener('change',function(event){
 let volumeSelected = event.target.value;
 videoElement.volume = volumeSelected;
+})
+
+speedControl.addEventListener('changed',function(event){
+let speedSelected = event.target.value;
+videoElement.playbackRate = speedSelected;
 })
