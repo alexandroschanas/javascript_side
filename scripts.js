41468ed279f0f7b558ebc47b3715f5394a376959
Your Name <you@example.com>
2025-01-24 16:47:43 +0200

    Creation of progress bar

--- scripts.js
+++ scripts.js
@@ -23,3 +23,14 @@ speedControl.addEventListener('change',function(event){
 let speedSelected = event.target.value;
 videoElement.playbackRate = speedSelected;
 })
+
+let totalVideoDuration = videoElement.duration;
+let totalProgressbar = document.querySelector(".progress");
+let incrementalProgressbar = document.querySelector(".progress__filled");
+let videoCurrentTime = videoElement.currentTime;
+
+videoCurrentTime.addEventListener('change',function(event){
+    console.log(videoCurrentTime);
+    console.log(incrementalProgressbar);
+})
+
